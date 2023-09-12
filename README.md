ec6af549f004e03da133c2f026a1c6169e21c38f
Martin Piron <88021434+ooftee@users.noreply.github.com>
2023-09-12 15:42:47 +1000

    GR

--- README.md
+++ README.md
@@ -1,25 +1,46 @@
 # dialog-starterkit
 
-This is a script to install apps in bulk using custom jamf triggers.
+In todays corporate world, the demand for expeditious processes is paramount, alongside a keen emphasis on customization. 
+Enter Starter Kits, a solution tailored to this need.
 
-Users have the option to skip any apps they don't want.
+Starter Kits offer a streamlined approach, enabling admin to use a single build profile for all computers. 
+Users are then empowered to efficiently install multiple applications with just a few clicks.
+
+This bash script allows users to install apps in bulk using custom jamf triggers with the option to skip any apps they don't want.
 
 ## Things to look for
 
-The working directory is `/Library/Management/Dialog-SK`
+The working directory is `/Library/Management/Dialog-SarterKit`
+This is where icons and logs will be stored.
+
+### Define your Array
+
+The `APPS` array is the mos important part of the script, it defines which apps you want to install and how to detect if it was installed succesfully or not.
+Each app requires 3 variables:
+- FriendlyName: THe name of the app that will be displayed in the list
+- Location: Where the app gets installed, this is used to validate the installation
+- JamfTrigger&IconName: Custom event trigger in jamf, also the icon name (see below)
+
+ie: iTerm,/Applications/iTerm.app,install_iterm
 
 ### Icons
 
 Icons are cashed locally, please provide a public URL to download them from.
 Make sure the icon name is the same as the jamf custom trigger.
 
+for example if your policy trigger is `install_iterm` make sure the icon is named `install_iterm.png`
+
 ### Branding
 
-Replace the icon argument in the `START_JSON` and `INSTALL_JSON` with your company logo.
+Due to the single quoting, variable can't be used in `START_JSON` and `INSTALL_JSON`.
+It is recommended to replace the icon argument in there with your company logo.
+Feel free to change message and title to fit your corporate messaging style.
 
 ### Requirements
 
-This script uses SwiftDialog and JQ, it requires the policies `install_swiftdialog` and `install_jq` to exists.
+This script uses SwiftDialog and JQ. 
+Please ensure those are installed in the /usr/local/bin folder.
+If the script is unable to find the binaries it will attempt to install them with these custom triggers `install_swiftdialog` and `install_jq`.
 
 <img width="932" alt="dialog_starterkit-01" src="https://github.com/ooftee/dialog-starterkit/assets/88021434/3298f471-8971-4f1a-ab04-1f3eea194401">
 <img width="932" alt="dialog_starterkit-02" src="https://github.com/ooftee/dialog-starterkit/assets/88021434/4138fb4e-e9f1-4304-b55a-f1b1bb7e3fbb">
--- dialog_starterkit.sh
+++ dialog_starterkit.sh
@@ -7,9 +7,9 @@
 #
 # Authors: Martin Piron
 #
-# Name:     dialog_starterkit.sh
-# Version:  1.0.0 (11-09-2023)
-# Repo:     https://github.com/ooftee/dialog-starterkit
+# Name:     dialog_starterKit.sh
+# Version:  1.0.0 (12-09-2023)
+# Repo:     https://github.com/ooftee/dialog-starterKit
 #
 # This script was inspired by Adam Codega's: https://github.com/acodega/dialog-scripts/
 
@@ -22,71 +22,69 @@
 # Lets users select which apps they want installed or not.
 
 #########################################################################################
-# CUSTOM VARIABLES TO EDIT
+# VARIABLES TO EDIT
 #########################################################################################
 
 # Apps to install (format is FriendlyName,Location,JamfTrigger&IconName)
-APPS=(
-  "GitHub Desktop,/Applications/GitHub Desktop.app,install_githubdesktop"
-  "Icons,/Applications/Icons.app,install_icons"
-  "Imazing Profile Editor,/Applications/iMazing Profile Editor.app,install_imazingprofileeditor"
-  "iTerm,/Applications/iTerm.app,install_iterm"
-  "Keka,/Applications/Keka.app,install_keka"
-)
+  APPS=(
+    "GitHub Desktop,/Applications/GitHub Desktop.app,install_githubdesktop"
+    "Icons,/Applications/Icons.app,install_icons"
+    "Imazing Profile Editor,/Applications/iMazing Profile Editor.app,install_imazingprofileeditor"
+    "iTerm,/Applications/iTerm.app,install_iterm"
+    "Keka,/Applications/Keka.app,install_keka"
+  )
 
 # Your Icon Public Bucket
-BUCKET="https://s3.ap-southeast-2.amazonaws.com/***"
+  BUCKET="https://s3.ap-southeast-2.amazonaws.com/***"
 
-# swiftDialog message (Update icon with path to your company logo)
-START_JSON='{
-  "title" : "Starter Kit",
-  "message" : "Please select the apps you want to install.\n\nScoll down to see them all.",
-  "messagefont" : "colour=#666666,weight=medium",
-  "icon" : "PATH_TO_LOGO",
-  "button1text" : "OK",
-  "button2text" : "Cancel",
-  "checkboxstyle": {
-    "style": "switch",
-    "size": "small"
-  }
-}'
-
-INSTALL_JSON='{
-  "title" : "Installing Apps",
-  "message" : "Please wait while we install apps:",
-  "messagefont" : "colour=#666666,weight=medium",
-  "icon" : "PATH_TO_LOGO",
-  "overlayicon" : "SF=arrow.down.circle.fill,palette=white,black,none,bgcolor=none",
-  "position" : "bottomright",
-  "button1text" : "Please wait"
-}'
+# swiftDialog message (Update icon with path to your company logo, title and message can be edited too)
+  START_JSON='{
+    "title" : "Starter Kit",
+    "message" : "Please select the apps you want to install.\n\nScoll down to see them all.",
+    "messagefont" : "colour=#666666,weight=medium",
+    "icon" : "PATH_TO_LOGO",
+    "button1text" : "OK",
+    "button2text" : "Cancel",
+    "checkboxstyle": {
+      "style": "switch",
+      "size": "small"
+    }
+  }'
+  
+  INSTALL_JSON='{
+    "title" : "Installing Apps",
+    "message" : "Please wait while we install apps:",
+    "messagefont" : "colour=#666666,weight=medium",
+    "icon" : "PATH_TO_LOGO",
+    "overlayicon" : "SF=arrow.down.circle.fill,palette=white,black,none,bgcolor=none",
+    "position" : "bottomright",
+    "button1text" : "Please wait"
+  }'
 
 #########################################################################################
 # SCRIPT VARIABLES
 #########################################################################################
 
 # location of dialog and dialog command file
-DIALOG_APP="/usr/local/bin/dialog"
-DIALOG_COMMAND_FILE="/var/tmp/dialog.log"
-START_JSON_FILE="/var/tmp/dialog_START_JSON.json"
-INSTALL_JSON_FILE="/var/tmp/dialog_INSTALL_JSON.json"
-CHECKED_JSON_FILE="/var/tmp/dialog_CHECKED_JSON.json"
+  DIALOG_APP="/usr/local/bin/dialog"
+  DIALOG_COMMAND_FILE="/var/tmp/dialog.log"
+  START_JSON_FILE="/var/tmp/dialog_START_JSON.json"
+  INSTALL_JSON_FILE="/var/tmp/dialog_INSTALL_JSON.json"
+  CHECKED_JSON_FILE="/var/tmp/dialog_CHECKED_JSON.json"
 
 # set progress total to the number of apps in the list
-PROGRESS_TOTAL=${#APPS[@]}
+  PROGRESS_TOTAL=${#APPS[@]}
 
 # Jamf binary
-JAMF="/usr/local/bin/jamf"
+  JAMF="/usr/local/bin/jamf"
 
 #########################################################################################
-# Testing and Logging
+# Logging
 #########################################################################################
-# Testing flag will enable the script logic to run without dependencies
-#TESTING_MODE="false" # false (default) or true to speed things up and write more logs
 
 # Enable logging to a file on disk and specify a directory
-ENABLE_LOGFILE="true" # false (default) or true to write the logs to a file
-LOGDIR="/Library/Management/Dialog-SK/Logs" # /var/tmp (default) or override by specifying a path
+  ENABLE_LOGFILE="true" # false (default) or true to write the logs to a file
+  LOGDIR="/Library/Management/Dialog-StarterKit/Logs" # /var/tmp (default) or override by specifying a path
 
 #########################################################################################
 # Global Functions
@@ -118,14 +116,14 @@ _init () {
 cleanup() {
   EXIT_CODE=$?
   # Create this function to perform clean up at exit
-  # exit_cleanup "${EXIT_CODE}" # not used
+  # exit_cleanup "${EXIT_CODE}" # implement as required
   info "## Finish: $(date +"%F %R:%S")"
   info "## Exit Code: ${EXIT_CODE}"
 }
 
 # Global Function Setup
-trap cleanup EXIT
-_init
+  trap cleanup EXIT
+  _init
 
 #########################################################################################
 # Script Functions
@@ -148,146 +146,146 @@ finalise(){
 #########################################################################################
 
 # Install swiftDialog if not installed
-if [[ ! -e "/Library/Application Support/Dialog/Dialog.app" ]]; then
-  warn "swiftDialog missing, installing"
-  "${JAMF}" policy -event install_swiftdialog
-else
-  info "swiftDialog already installed"
-fi
+  if [[ ! -e "/Library/Application Support/Dialog/Dialog.app" ]]; then
+    warn "swiftDialog missing, installing"
+    "${JAMF}" "policy" -event "install_swiftdialog"
+  else
+    info "swiftDialog already installed"
+  fi
 
 # Install jq if not installed
-if [[ ! -e "/usr/local/bin/jq" ]]; then
-  warn "JQ missing, installing"
-  "${JAMF}" policy -event install_jq
-else
-  info "JQ already installed"
-fi
+  if [[ ! -e "/usr/local/bin/jq" ]]; then
+    warn "JQ missing, installing"
+    "${JAMF}" "policy" -event "install_jq"
+  else
+    info "JQ already installed"
+  fi
 
 # Create icons folder and download them
-if [[ ! -e "/Library/Management/Dialog-SK/Branding/Icons/" ]]; then
-  info "Creating Icons folder"
-  mkdir -p "/Library/Management/Dialog-SK/Branding/Icons/"
-else
-  info "Icons folder already exists"
-fi
-
-for APP in "${APPS[@]}"; do
-  APP_NAME=$(echo "$APP" | cut -d ',' -f1)
-  APP_TRIGGER=$(echo "$APP" | cut -d ',' -f3)
-  if [[ ! -e "/Library/Management/Dialog-SK/Branding/Icons/${APP_TRIGGER}.png" ]]; then
-    warn "${APP_NAME} icon not found, downloading"
-    curl "${BUCKET}${APP_TRIGGER}.png" -o "/Library/Management/Dialog-SK/Branding/Icons/${APP_TRIGGER}.png"
-  elif [[ -e "/Library/Management/Dialog-SK/Branding/Icons/${APP_TRIGGER}.png" ]]; then
-    info "${APP_NAME} icon already cached"
+  if [[ ! -e "/Library/Management/Dialog-SarterKit/Branding/Icons/" ]]; then
+    info "Creating Icons folder"
+    mkdir -p "/Library/Management/Dialog-SarterKit/Branding/Icons/"
+  else
+    info "Icons folder already exists"
   fi
-done
+  
+  for APP in "${APPS[@]}"; do
+    APP_NAME=$(echo "$APP" | cut -d ',' -f1)
+    APP_TRIGGER=$(echo "$APP" | cut -d ',' -f3)
+    if [[ ! -e "/Library/Management/Dialog-SarterKit/Branding/Icons/${APP_TRIGGER}.png" ]]; then
+      warn "${APP_NAME} icon not found, downloading"
+      curl "${BUCKET}${APP_TRIGGER}.png" -o "/Library/Management/Dialog-SarterKit/Branding/Icons/${APP_TRIGGER}.png"
+    elif [[ -e "/Library/Management/Dialog-SarterKit/Branding/Icons/${APP_TRIGGER}.png" ]]; then
+      info "${APP_NAME} icon already cached"
+    fi
+  done
 
 # Create the list of apps and format it in json
-CHECKBOX_JSON=$(printf '%s\n' "${APPS[@]}" | awk -F ',' '{printf "{\"label\":\"%s\",\"checked\":true,\"icon\":\"/Library/Management/Dialog-SK/Branding/Icons/%s.png\"}\n", $1, $3}' | jq -s '{"checkbox": .}')
-
-LISTITEMS_JSON=$(printf '%s\n' "${APPS[@]}" | awk -F ',' '{printf "{\"title\":\"%s\",\"icon\":\"/Library/Management/Dialog-SK/Branding/Icons/%s.png\",\"status\":\"pending\",\"statustext\":\"Pending\"}\n", $1, $3}' | jq -s '{"listitem": .}')
+  CHECKBOX_JSON=$(printf '%s\n' "${APPS[@]}" | awk -F ',' '{printf "{\"label\":\"%s\",\"checked\":true,\"icon\":\"/Library/Management/Dialog-SarterKit/Branding/Icons/%s.png\"}\n", $1, $3}' | jq -s '{"checkbox": .}')
+  
+  LISTITEMS_JSON=$(printf '%s\n' "${APPS[@]}" | awk -F ',' '{printf "{\"title\":\"%s\",\"icon\":\"/Library/Management/Dialog-SarterKit/Branding/Icons/%s.png\",\"status\":\"pending\",\"statustext\":\"Pending\"}\n", $1, $3}' | jq -s '{"listitem": .}')
 
 # Merge json variables into one file
-START_MERGED_JSON=$(jq -n --argjson START_JSON "${START_JSON}" --argjson CHECKBOX_JSON "${CHECKBOX_JSON}" '$START_JSON + $CHECKBOX_JSON')
-echo "${START_MERGED_JSON}" > "${START_JSON_FILE}"
-
-INSTALL_MERGED_JSON=$(jq -n --argjson INSTALL_JSON "${INSTALL_JSON}" --argjson LISTITEMS_JSON "${LISTITEMS_JSON}" '$INSTALL_JSON + $LISTITEMS_JSON') 
-echo "${INSTALL_MERGED_JSON}" > "${INSTALL_JSON_FILE}"
+  START_MERGED_JSON=$(jq -n --argjson START_JSON "${START_JSON}" --argjson CHECKBOX_JSON "${CHECKBOX_JSON}" '$START_JSON + $CHECKBOX_JSON')
+  echo "${START_MERGED_JSON}" > "${START_JSON_FILE}"
+  
+  INSTALL_MERGED_JSON=$(jq -n --argjson INSTALL_JSON "${INSTALL_JSON}" --argjson LISTITEMS_JSON "${LISTITEMS_JSON}" '$INSTALL_JSON + $LISTITEMS_JSON') 
+  echo "${INSTALL_MERGED_JSON}" > "${INSTALL_JSON_FILE}"
 
 # Launch Dialog and display the lsit of apps available to install
-DIALOG_CMD="${DIALOG_APP} 
---jsonfile ${START_JSON_FILE} \
---json"
+  DIALOG_CMD="${DIALOG_APP} 
+  --jsonfile ${START_JSON_FILE} \
+  --json"
 
-info "Launching Dialog - App Select"
-APPS_SELECTED=$($DIALOG_CMD)
+  info "Launching Dialog - App Select"
+  APPS_SELECTED=$($DIALOG_CMD)
 
 # Exit if user clicked cancel (array is empty)
-if [[ ! ${APPS_SELECTED[@]} ]]; then
- fatal "User clicked cancel."
-fi
+  if [[ ! "${APPS_SELECTED[*]}" ]]; then
+   fatal "User clicked cancel."
+  fi
 
 # Read the JSON object and construct an array of key-value pairs
-APPS_SELECTED_values=()
-while IFS= read -r line; do
-  APPS_SELECTED_values+=("$line")
-done < <(echo "${APPS_SELECTED}" | jq -r 'to_entries | map("\(.key)=\(.value)")[]')
+  APPS_SELECTED_values=()
+  while IFS= read -r line; do
+    APPS_SELECTED_values+=("$line")
+  done < <(echo "${APPS_SELECTED}" | jq -r 'to_entries | map("\(.key)=\(.value)")[]')
 
 # Create array with all the apps to install
-INSTALL_IS_TRUE=()
-for APP in "${APPS_SELECTED_values[@]}"; do
-  APP_NAME=$(echo "${APP}" | cut -d '=' -f1)
-  VALUE=$(echo "${APP}" | cut -d '=' -f2)
-  if [[ "${VALUE}" == "true" ]]; then
-    info "${APP_NAME} will be installed"
-    INSTALL_IS_TRUE+=("${APP_NAME}")
-  elif [[ "${VALUE}" == "false" ]]; then
-    info "${APP_NAME} will be skipped"
-    # Update the status and statustext of skipped apps in the JSON file
-    /usr/local/bin/jq --arg app "${APP_NAME}" '.listitem |= map(if .title == $app then .status = "fail" | .statustext = "Skipping" else . end)' "${INSTALL_JSON_FILE}" > "${CHECKED_JSON_FILE}"
-    # Rename the updated file back to the original one
-    mv "${CHECKED_JSON_FILE}" "${INSTALL_JSON_FILE}"
-  fi
-done
+  INSTALL_IS_TRUE=()
+  for APP in "${APPS_SELECTED_values[@]}"; do
+    APP_NAME=$(echo "${APP}" | cut -d '=' -f1)
+    VALUE=$(echo "${APP}" | cut -d '=' -f2)
+    if [[ "${VALUE}" == "true" ]]; then
+      info "${APP_NAME} will be installed"
+      INSTALL_IS_TRUE+=("${APP_NAME}")
+    elif [[ "${VALUE}" == "false" ]]; then
+      info "${APP_NAME} will be skipped"
+      # Update the status and statustext of skipped apps in the JSON file
+      /usr/local/bin/jq --arg app "${APP_NAME}" '.listitem |= map(if .title == $app then .status = "fail" | .statustext = "Skipping" else . end)' "${INSTALL_JSON_FILE}" > "${CHECKED_JSON_FILE}"
+      # Rename the updated file back to the original one
+      mv "${CHECKED_JSON_FILE}" "${INSTALL_JSON_FILE}"
+    fi
+  done
 
 # Update Dialog with the install json file
-DIALOG_CMD="${DIALOG_APP} \
---progress ${PROGRESS_TOTAL} \
---button1disabled \
---jsonfile $INSTALL_JSON_FILE"
-
-STEP_PROGRESS=0
-
-info "Launching Dialog - App Install"
-eval "$DIALOG_CMD" & sleep 2
+  DIALOG_CMD="${DIALOG_APP} \
+  --progress ${PROGRESS_TOTAL} \
+  --button1disabled \
+  --jsonfile $INSTALL_JSON_FILE"
+  
+  STEP_PROGRESS=0
+  
+  info "Launching Dialog - App Install"
+  eval "$DIALOG_CMD" & sleep 2
  
 # Loop thru array and install apps if they were selected
-for APP in "${APPS[@]}"; do
-  APP_NAME=$(echo "${APP}" | cut -d ',' -f1)
-  APP_TRIGGER=$(echo "${APP}" | cut -d ',' -f3)
-  APP_LOCATION=$(echo "${APP}" | cut -d ',' -f2)
-  STEP_PROGRESS=$(( 1 + STEP_PROGRESS ))
-  dialog_command "progress: ${STEP_PROGRESS}"
-  STATUS=$(jq --arg app "${APP_NAME}" '.listitem[] | select(.title == $app) | .statustext' "${INSTALL_JSON_FILE}" | tr -d '"')
-  if [[ "${STATUS}" = "Pending" ]]; then
-    if [[ ! -e "${APP_LOCATION}" ]]; then
-      info "${APP_NAME} not installed, installing"
-      dialog_command "listitem: ${APP_NAME}: wait"
-      dialog_command "progresstext: Installing ${APP_NAME}"
-      "${JAMF}" policy -event "${APP_TRIGGER}"
-      sleep 1
-      if [[ -e "${APP_LOCATION}" ]]; then
+  for APP in "${APPS[@]}"; do
+    APP_NAME=$(echo "${APP}" | cut -d ',' -f1)
+    APP_TRIGGER=$(echo "${APP}" | cut -d ',' -f3)
+    APP_LOCATION=$(echo "${APP}" | cut -d ',' -f2)
+    STEP_PROGRESS=$(( 1 + STEP_PROGRESS ))
+    dialog_command "progress: ${STEP_PROGRESS}"
+    STATUS=$(jq --arg app "${APP_NAME}" '.listitem[] | select(.title == $app) | .statustext' "${INSTALL_JSON_FILE}" | tr -d '"')
+    if [[ "${STATUS}" = "Pending" ]]; then
+      if [[ ! -e "${APP_LOCATION}" ]]; then
+        info "${APP_NAME} not installed, installing"
+        dialog_command "listitem: ${APP_NAME}: wait"
+        dialog_command "progresstext: Installing ${APP_NAME}"
+        "${JAMF}" policy -event "${APP_TRIGGER}"
+        sleep 1
+        if [[ -e "${APP_LOCATION}" ]]; then
+          info "${APP_NAME} installed"
+          dialog_command "listitem: ${APP_NAME}: success"
+        else
+          error "${APP_NAME} Failed"
+          dialog_command "listitem: ${APP_NAME}: error"
+          FAILED_STATUS+=("${APP_TRIGGER}")
+        fi
+      elif [[ -e "${APP_LOCATION}" ]]; then
         info "${APP_NAME} installed"
+        dialog_command "progresstext: ${APP_NAME} is already installed"
         dialog_command "listitem: ${APP_NAME}: success"
       else
         error "${APP_NAME} Failed"
         dialog_command "listitem: ${APP_NAME}: error"
         FAILED_STATUS+=("${APP_TRIGGER}")
       fi
-    elif [[ -e "${APP_LOCATION}" ]]; then
-      info "${APP_NAME} installed"
-      dialog_command "progresstext: ${APP_NAME} is already installed"
-      dialog_command "listitem: ${APP_NAME}: success"
-    else
-      error "${APP_NAME} Failed"
-      dialog_command "listitem: ${APP_NAME}: error"
-      FAILED_STATUS+=("${APP_TRIGGER}")
+    elif [[ "${STATUS}" = "Skipping" ]]; then
+      warn "${APP_NAME} was NOT selected by the user to be installed."
+      dialog_command "progresstext: Skipping ${APP_NAME}"
     fi
-  elif [[ "${STATUS}" = "Skipping" ]]; then
-    warn "${APP_NAME} was NOT selected by the user to be installed."
-    dialog_command "progresstext: Skipping ${APP_NAME}"
-  fi
-  sleep 1
-done
+    sleep 1
+  done
 
 # Loop through failed array and report back if required
-if [[ ${#FAILED_STATUS[@]} -eq 0 ]] ; then
-  info "All apps installed."
-  dialog_command "progresstext: All done!"
-else
-  error "Failed: ${FAILED_STATUS[*]}"
-  dialog_command "progresstext: Some installations have failed..."
-fi
+  if [[ ${#FAILED_STATUS[@]} -eq 0 ]] ; then
+    info "All apps installed."
+    dialog_command "progresstext: All done!"
+  else
+    error "Failed: ${FAILED_STATUS[*]}"
+    dialog_command "progresstext: Some installations have failed..."
+  fi
 
 # Finishing up
-finalise
+  finalise
